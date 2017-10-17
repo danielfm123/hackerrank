4af4d266af1bec8e7fa035ff3517c654d24f1677
dfischer@entel.cl <dfischer@entel.cl>
2017-10-17 19:46:10 -0300

    passenger traffic submission

--- forecasting-passenger-traffic/code.R
+++ forecasting-passenger-traffic/code.R
@@ -1,8 +1,14 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 # https://www.hackerrank.com/challenges/forecasting-passenger-traffic/problem
-input = "data.txt"
-# input = "stdin"
+# input = "forecasting-passenger-traffic/data.txt"
+# input = "forecasting-passenger-traffic/data2.txt"
+input = "stdin"
 data <- suppressWarnings(read.table(input,skip = 1))
-colnames(data) = c("period","passeners")
-forecast = c(1563178,1312558,1501793,1388316,1325942,1410769,687396,1493945,1161128,590382,1082215,1416327)
+colnames(data) = c("period","value")
+data$month = as.character(1:nrow(data) %% 12)
+data$trend = 1:nrow(data)
+fit = lm(value ~ month + trend,data)
+
+predict_dataset = data.frame(month = as.character((nrow(data)+1):(nrow(data)+12) %% 12) ,
+                             trend = (nrow(data)+1):(nrow(data)+12))
+forecast = round(predict(fit,predict_dataset))
 writeLines(as.character(forecast))
