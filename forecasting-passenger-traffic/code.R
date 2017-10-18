01cac85f5c1f65846b5cb56ac2b891a322639769
ENTEL\dfischer <dfischer@entel.cl>
2017-10-18 15:03:26 -0300

    merged

--- forecasting-passenger-traffic/code.R
+++ forecasting-passenger-traffic/code.R
@@ -1,12 +1,7 @@
 # https://www.hackerrank.com/challenges/forecasting-passenger-traffic/problem
-<<<<<<< HEAD
-input = "forecasting-passenger-traffic/data.txt"
-# input = "stdin"
-=======
 # input = "forecasting-passenger-traffic/data.txt"
 # input = "forecasting-passenger-traffic/data2.txt"
 input = "stdin"
->>>>>>> 4af4d266af1bec8e7fa035ff3517c654d24f1677
 data <- suppressWarnings(read.table(input,skip = 1))
 colnames(data) = c("period","value")
 data$month = as.character(1:nrow(data) %% 12)
